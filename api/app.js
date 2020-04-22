bfc211673cd48804c097f8eddf9752f57408dbd2
Jose <chaparro.jjr@gmail.com>
2020-04-21 19:42:28 -0700

    front end is now connected to the back end, data within the json file now appears on the blog page

--- api/app.js
+++ api/app.js
@@ -1,8 +1,28 @@
 const express = require("express");
 const app = express();
+const Post = require("./api/models/posts");
+const postData = new Post;
 
-app.get("/", (req, res) => {
-   res.status(200).send("Hello World")
+
+app.use((req, res, next) => {
+   res.setHeader("Access-Control-Allow-Origin", "*");
+   next();
+});
+
+app.use('/uploads', express.static('uploads'));
+
+app.get("/api/posts", (req, res) => {
+   res.status(200).send(postData.get());
+});
+
+app.get("/api/posts/:post_id", (req, res) => {
+   const postId = req.params.post_id;
+   const foundPost = postData.getIndividualPost(postId);
+   if (foundPost) {
+      res.status(200).send(foundPost);
+   } else {
+      res.status(404).send('Not Found');
+   }
 });
 
 app.listen(3000, () => console.log("Now listening on http://localhost:3000"));
