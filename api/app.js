4734ce33131ee70e87ff410bf979f90228fd9a98
Jose <chaparro.jjr@gmail.com>
2020-04-21 20:54:52 -0700

    api end point for uploading data to the json file works. Multer set up complete

--- api/app.js
+++ api/app.js
@@ -1,8 +1,30 @@
 const express = require("express");
 const app = express();
 const Post = require("./api/models/posts");
+const multer = require('multer');
+const storage = multer.diskStorage({
+   destination: function (req, file, cb) {
+      cb(null, './uploads')
+   },
+   filename: function (req, file, cb) {
+      cb(null, `${file.fieldname}-${Date.now()}${getExt(file.mimetype)}`);
+   }
+});
+
+const getExt = (mimeType) => {
+   switch (mimeType) {
+      case "image/png":
+         return ".png";
+      case "image/jpeg":
+         return ".jpeg";
+   }
+};
+  
+const upload = multer({ storage: storage })
+
 const postData = new Post;
 
+app.use(express.json());
 
 app.use((req, res, next) => {
    res.setHeader("Access-Control-Allow-Origin", "*");
@@ -24,5 +46,18 @@ app.get("/api/posts/:post_id", (req, res) => {
       res.status(404).send('Not Found');
    }
 });
+
+app.post("/api/posts", upload.single("post-image"), (req, res) => {
+   const newPost =
+   {
+      "id": `${Date.now()}`,
+      "title": req.body.title,
+      "content": req.body.content,
+      "post_image": req.file.path,
+      "added_date": `${Date.now()}`
+   }
+   postData.add(newPost);
+   res.status(200).send(newPost);
+})
 
 app.listen(3000, () => console.log("Now listening on http://localhost:3000"));
